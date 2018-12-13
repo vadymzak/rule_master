97c846fb4d19e116044469ecedee7d724114da09
vadymzak <vadim.if@gmail.com>
2018-12-13 19:31:27 +0200

    change redis storage

--- Gemfile
+++ Gemfile
@@ -1,7 +1,5 @@
 source 'https://rubygems.org'
 
-ruby '2.4.3'
-
 gem 'pry'
 gem 'rack-test'
 gem 'redis'
--- lib/redis_storage.rb
+++ lib/redis_storage.rb
@@ -6,23 +6,18 @@ class RedisStorage < Storage
   def initialize
     @redis_rules = Redis.new
 
-    @redis_rules.hset('report_in_company_bp', :resource, 'report_in_company_bp')
-    @redis_rules.hset('report_in_company_bp', :description,
-                'allow access to bp report')
-    @redis_rules.hset('report_in_company_bp', :params, { id: 123, bp_id: 'abc123',
-                                                   report_id: 128 })
-    @redis_rules.hset('report_in_company_bp', :participant, id: 123)
-    @redis_rules.hset('report_in_company_bp', :methods, 'GET')
-    @redis_rules.hset('report_in_company_bp', :action, 'allow')
-
-    @redis_rules.hset('second_resourse', :resource, 'second_resourse')
-    @redis_rules.hset('second_resourse', :description,
-                'allow access to second resourse')
-    @redis_rules.hset('second_resourse', :params, { id: 123, bp_id: 'abc123',
-                                              report_id: 128 })
-    @redis_rules.hset('second_resourse', :participant, id: 123)
-    @redis_rules.hset('second_resourse', :methods, 'GET')
-    @redis_rules.hset('second_resourse', :action, 'allow')
+    @redis_rules.hmset('report_in_company_bp_new',:resource, 'report_in_company_bp',
+                                                  :description, 'allow access to bp report',
+                                                  :params, { id: 123, bp_id: 'abc123', report_id: 128 },
+                                                  :participant, id: 123,
+                                                  :methods => 'GET',
+                                                  :action => 'allow')
+    @redis_rules.hmset('second_resourse1',  :resource, 'second_resourse1',
+                                            :description, 'allow access to second resourse',
+                                            :params, { id: 123, bp_id: 'abc123', report_id: 128 },
+                                            :participant, id: 123,
+                                            :methods => 'GET',
+                                            :action => 'allow')
   end
 
   def find_rule(rule_name)
