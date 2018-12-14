b90b69e6242139e9df90c6cec2b8344321cea3a4
vadymzak <vadim.if@gmail.com>
2018-12-14 15:44:08 +0200

    fix redis storage

--- lib/redis_storage.rb
+++ lib/redis_storage.rb
@@ -9,15 +9,16 @@ class RedisStorage < Storage
     @redis_rules.hmset('report_in_company_bp_new',:resource, 'report_in_company_bp',
                                                   :description, 'allow access to bp report',
                                                   :params, { id: 123, bp_id: 'abc123', report_id: 128 },
-                                                  :participant, id: 123,
-                                                  :methods => 'GET',
-                                                  :action => 'allow')
+                                                  :participant, {id: 123},
+                                                  :methods, 'GET',
+                                                  :action, 'allow')
     @redis_rules.hmset('second_resourse1',  :resource, 'second_resourse1',
                                             :description, 'allow access to second resourse',
                                             :params, { id: 123, bp_id: 'abc123', report_id: 128 },
-                                            :participant, id: 123,
-                                            :methods => 'GET',
-                                            :action => 'allow')
+                                            :participant, {id: 123},
+                                            :methods, 'GET',
+                                            :action,  'allow')
+    p @redis_rules.hgetall('report_in_company_bp_new')
   end
 
   def find_rule(rule_name)
